14de319bb2da2ba5d4db3e8c0c4e0330502e13d2
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-03-02 15:54:29 -0500

    .

--- CalibrationSrf02.py
+++ CalibrationSrf02.py
@@ -149,6 +149,9 @@ def Variation_Coef(signal):
   else:
       return 0
 
+def Fft_energy(signal):
+  return np.abs(np.fft.fft(signal))**2
+
 def getStats(values, field):
   results = {"min": values[0][field], 
              "max": values[0][field],
@@ -210,23 +213,24 @@ with open("data.csv", 'w', newline='') as file:
     if(opc == "1"):
       sensed,distance,mindistance,TimeElapse = s.getValues(100)
       rangeStats =  getStats(sensed, "distance")
-      print("distance_min"+str(rangeStats["min"]))
-      print("distance_max"+str(rangeStats["max"]))
-      print("distance_mean"+str(rangeStats["mean"]))
-      print("distance_delta"+str(rangeStats["distanceDelta"]))
-      print("Variance"+str(Variance(distance)))
-      print("St_des"+str(St_des(distance)))
+      print("distance_min "+str(rangeStats["min"]))
+      print("distance_max "+str(rangeStats["max"]))
+      print("distance_mean "+str(rangeStats["mean"]))
+      print("distance_delta "+str(rangeStats["distanceDelta"]))
+      print("Variance "+str(Variance(distance)))
+      print("St_des "+str(St_des(distance)))
       #print(str(Mean(distance))+"\n")
-      print("Autocorrelation"+str(Autocorrelation(distance)))
-      print("Above_Threshold"+str(Above_Threshold(distance)))
-      print("Above_Median"+str(Above_Median(distance)))
-      print("Below_Threshold"+str(Below_Threshold(distance)))
-      print("Below_Median"+str(Below_Median(distance)))
+      print("Autocorrelation "+str(Autocorrelation(distance)))
+      print("Above_Threshold "+str(Above_Threshold(distance)))
+      print("Above_Median "+str(Above_Median(distance)))
+      print("Below_Threshold "+str(Below_Threshold(distance)))
+      print("Below_Median "+str(Below_Median(distance)))
       #print(str(Mean_Abs(Mean_Abs))+"\n")
-      print("Crossing_M"+str(Crossing_M(distance)))
+      print("Crossing_M "+str(Crossing_M(distance)))
       #print(str(Number_Peak(distance))+"\n")
-      print("Sample_Entropy"+str(Sample_Entropy(distance)))
-      print("Variation_Coef"+str(Variation_Coef(distance)))
+      print("Sample_Entropy "+str(Sample_Entropy(distance)))
+      print("Variation_Coef "+str(Variation_Coef(distance)))
+      print("Fft power "+str(Fft_energy(distance)))
       #writer.writerow([rangeStats["min"], rangeStats["max"], rangeStats["mean"],rangeStats["distanceDelta"],Variance(distance),St_des(distance),Mean(distance),Autocorrelation(distance),Above_Threshold(distance),Above_Median(distance),Below_Threshold(distance),Below_Median(distance),Mean_Abs(Mean_Abs),Crossing_M(distance),Number_Peak(distance),Sample_Entropy(distance),Variation_Coef(distance),1])
     #Get data for not presence
     if(opc == "2"):
