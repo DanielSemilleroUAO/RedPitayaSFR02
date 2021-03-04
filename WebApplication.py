38445a708188249307d8b4a2e8fd4e0607890f2a
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-03-04 10:36:27 -0500

    .

--- WebApplication.py
+++ WebApplication.py
@@ -11,7 +11,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title><meta http-equiv=""refresh"" content=""30""></head>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title><meta http-equiv=\"refresh\" content=\"30\"></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server"+conteo+".</p>", "utf-8"))
