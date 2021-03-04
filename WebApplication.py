82a9695d0ce2e07955debd30fb529b4d72448979
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-03-04 11:39:53 -0500

    .

--- WebApplication.py
+++ WebApplication.py
@@ -4,19 +4,20 @@ import time
 
 hostName = "192.168.1.225"
 serverPort = 400
-conteo = 0
 
 class MyServer(BaseHTTPRequestHandler):
 
+    self.conteo = 0
+
     def do_GET(self):
-        conteo = conteo+1
+        self.conteo = self.conteo+1
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title><meta http-equiv=\"refresh\" content=\"30\"></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an "+str(conteo)+".</p>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an "+str(self.conteo)+".</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
