4f53924a37410d093ecd1f7f3e01991ac87310cd
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2021-03-04 11:01:54 -0500

    .

--- WebApplication.py
+++ WebApplication.py
@@ -4,17 +4,16 @@ import time
 
 hostName = "192.168.1.225"
 serverPort = 400
-conteo = 0
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title><meta http-equiv=\"refresh\" content=\"30\"></head>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server"+conteo+".</p>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
@@ -22,8 +21,9 @@ if __name__ == "__main__":
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        conteo+=1
         webServer.serve_forever()
     except KeyboardInterrupt:
-        webServer.server_close()
-        print("Server stopped.")
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
